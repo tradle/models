8b62c274e1d22d2aff77b482d19d61996c639161
pgmemk <ellen.katsnelson@lablz.com>
2016-03-13 19:09:23 +0100

    generalizing model validation for one and/or many models

--- validator.js
+++ validator.js
@@ -20,118 +20,23 @@ Validator.validateReferences = function (models) {
 }
 
 proto.validate = function (json) {
-  let err = []
-  // _t
-  if (!json.type) {
-    err.push('the required property type "tradle.Model" is missing')
-    first = true
-  }
-  else if (json.type !== 'tradle.Model') {
-    err.push('the required property  "type" can have only "tradle.Model" value')
-    first = true
-  }
-  // id
-  if (!json.id)
-    err.push('the required property "id" is missing. It should have value like "tradle.[modelName]" like for example "tradle.Mortgage"')
-  else if (typeof json.id !== 'string')
-    err.push('the required property "id" should be of type string. It should have value like "tradle.[modelName]" like for example "tradle.Mortgage"')
-
-  // subClassOf
-  if (json.subClassOf) {
-    let isFinancialProduct = json.subClassOf === 'tradle.FinancialProduct'
-    // if (!models[json.subClassOf])
-    //   err.push('there is no model with the "id" ' + json.subClassOf)
-    // else
-    if (isFinancialProduct) {
-      if (!json.forms)
-        err.push('the model that is a subClassOf "tradle.FinancialProduct" should have property "forms" as an array of models that have subClassOf "tradle.Form" property')
-      else if (json.forms.constructor === Array)
-        err.push('"forms" property of the models that is subClassOf "tradle.FinancialProduct" should be an array of valid models that are subClassOf "tradle.Form"')
-    }
-    if (json.interfaces  &&  !json.interfaces[0]) {
-      err.push('the model that is a subClassOf "tradle.FinancialProduct" should have property "forms" as an array of models that have subClassOf "tradle.Form" property')
-    }
-  }
-
-  // required
-  // if (!json.required)
-  //   err.push('property "required" should be defined in model and the value for it should be an array of property names')
-  // else
-  if (json.required  &&  (json.required.constructor !== Array))
-    err.push('the required property required should be of type Array')
-
-  // properties
-  if (!json.properties)
-    err.push('the required property "properties" is missing')
-  else if (typeof json.properties !== 'object')
-    err.push('the required property "properties" should be a valid json object')
-  else
-    err.push.apply(err, this.validateProperties(json))
-
-  return err
+  let errs = []
+  errs.push.apply(errs, this.validateModel(json))
+  errs.push.apply(errs, this.validateProperties(json))
+  return errs
 }
 
 proto.validateReferences = function (models) {
   models = models || this._models
 
-  const err = []
+  let errs = []
   const modelsO = this._byId
   models.forEach(function(m) {
-    if (m.subClassOf) {
-      if (!modelsO[m.subClassOf])
-        err.push('model "'+ m.id + '" has subClassOf that does not correspond to any model')
-      else {
-        let isFinancialProduct = m.subClassOf === 'tradle.FinancialProduct'
-        if (isFinancialProduct) {
-          if (!m.forms)
-            err.push('model "'+ m.id + '" has subClassOf "tradle.FinancialProduct". Being that it has to have property "forms"')
-
-          else {
-            m.forms.forEach(function(f) {
-              if (!modelsO[f])
-                err.push('there is no model with id "' + f + '"')
-              else if (!modelsO[f].subClassOf)
-                err.push('forms list for "' + m.id + '" contains "' + f +'" that is not subClassOf "tradle.Form"')
-            })
-          }
-        }
-        else if (m.subClassOf === 'tradle.Form') {
-          if (!m.interfaces)
-            err.push('' + m.id + ' is a subClassOf "tradle.Form". It should also implement interface "tradle.Message" and it\'s properties "from" and "to"')
-          else if (m.interfaces.constructor !== Array)
-            err.push('' + m.id + ' has property interfaces that should be an array')
-          else if (m.interfaces.indexOf('tradle.Message') === -1)
-            err.push('' + m.id + ' is a subClassOf "tradle.Form". It should also implement interface "tradle.Message" and it\'s properties "from" and "to"')
-        }
-      }
-    }
-    if (m.interfaces) {
-      if (m.interfaces.constructor !== Array)
-        err.push('' + m.id + ' has property interfaces that should be an array')
-      m.interfaces.forEach(function(i) {
-        if (!modelsO[i])
-          err.push('' + m.id + ' has property interfaces that lists model "' + i + '". This interface does not correspond to any model')
-        else if (!modelsO[i].isInterface)
-          err.push('' + m.id + ' has property interfaces that lists model "' + i + '". This interface model does not have a property "isInterface" that identifies it as interface')
-        else {
-          var req = modelsO[i].required
-          if (req) {
-            let rProps = ''
-            req.forEach(function(r) {
-              if (!m.properties[r])
-                rProps += '' + r
-            })
-            if (rProps.length)
-              err.push('  ' + m.id + ' implements interface "' + i + '". This interface requires implementing properties: ' + rProps.substring(2))
-          }
-        }
-      })
-    }
-
-    err.push.apply(err, this.validateProperties(m, true))
+    errs.push.apply(errs, this.validateModel(m, modelsO))
+    errs.push.apply(errs, this.validateProperties(m, true))
   }, this)
 
-  return err
+  return errs
 }
 
 proto.validateProperties = function (m, checkRefs) {
@@ -139,14 +44,23 @@ proto.validateProperties = function (m, checkRefs) {
 // ref
 // array
 // items or ref
+  // properties
+  let err = []
+  if (!m.properties) {
+    err.push('the required property "properties" is missing')
+    return err
+  }
+  else if (typeof m.properties !== 'object') {
+    err.push('the required property "properties" should be a valid json object')
+    return err
+  }
   const properties = m.properties
   const modelsO = this._byId
-  const err = []
   for (let p in properties) {
     let prop = properties[p]
     if (prop.readOnly) {
       if (prop.readOnly !== true && prop.readOnly !== false)
-        err.push('"' + m.id + '" property "' + p + '" defined as readOnly. readOnly can have only true/false values')
+        err.push('\n "' + m.id + '" property "' + p + '" defined as readOnly. readOnly can have only true/false values')
     }
     if (prop.type === 'object') {
       if (!prop.ref)
@@ -183,6 +97,81 @@ proto.validateProperties = function (m, checkRefs) {
         err.push('"' + m.id + '" property "' + p + '" has a units. It is allowed only for properties of type "number" or if the type of the property is "object" then it should have "ref" to "tradle.Money"')
     }
   }
+
+  return err
+}
+proto.validateModel = function(m, modelsO) {
+  let checkRefs = modelsO !== null
+  let err = []
+  if (!m.type) {
+    err.push('the required property type "tradle.Model" is missing')
+    first = true
+  }
+  else if (m.type !== 'tradle.Model') {
+    err.push('the required property  "type" can have only "tradle.Model" value')
+    first = true
+  }
+  // id
+  if (!m.id)
+    err.push('the required property "id" is missing. It should have value like "tradle.[modelName]" like for example "tradle.Mortgage"')
+  else if (typeof m.id !== 'string')
+    err.push('the required property "id" should be of type string. It should have value like "tradle.[modelName]" like for example "tradle.Mortgage"')
+  if (m.subClassOf) {
+    if (checkRefs  &&  !modelsO[m.subClassOf])
+      err.push('model "'+ m.id + '" has subClassOf that does not correspond to any model')
+    else {
+      let isFinancialProduct = m.subClassOf === 'tradle.FinancialProduct'
+      if (isFinancialProduct) {
+        if (!m.forms)
+          err.push('model "'+ m.id + '" has subClassOf "tradle.FinancialProduct". Being that it has to have property "forms"')
+
+        else if (checkRefs) {
+          m.forms.forEach(function(f) {
+            if (!modelsO[f])
+              err.push('there is no model with id "' + f + '"')
+            else if (!modelsO[f].subClassOf)
+              err.push('forms list for "' + m.id + '" contains "' + f +'" that is not subClassOf "tradle.Form"')
+          })
+        }
+        err.push('the model that is a subClassOf "tradle.FinancialProduct" should also implement "tradle.Message" interface to allow resources of this type show up in chat')
+      }
+      else if (m.subClassOf === 'tradle.Form') {
+        if (!m.interfaces)
+          err.push('' + m.id + ' is a subClassOf "tradle.Form". It should also implement interface "tradle.Message" and it\'s properties "from" and "to"')
+        else if (m.interfaces.constructor !== Array)
+          err.push('' + m.id + ' has property interfaces that should be an array')
+        else if (m.interfaces.indexOf('tradle.Message') === -1)
+          err.push('' + m.id + ' is a subClassOf "tradle.Form". It should also implement interface "tradle.Message" and it\'s properties "from" and "to"')
+      }
+    }
+  }
+
+  if (m.interfaces) {
+    if (m.interfaces.constructor !== Array)
+      err.push('' + m.id + ' has property interfaces that should be an array')
+    if (checkRefs) {
+      m.interfaces.forEach(function(i) {
+        if (!modelsO[i])
+          err.push('' + m.id + ' has property interfaces that lists model "' + i + '". This interface does not correspond to any model')
+        else if (!modelsO[i].isInterface)
+          err.push('' + m.id + ' has property interfaces that lists model "' + i + '". This interface model does not have a property "isInterface" that identifies it as interface')
+        else {
+          var req = modelsO[i].required
+          if (req) {
+            let rProps = ''
+            req.forEach(function(r) {
+              if (!m.properties[r])
+                rProps += '\n ' + r
+            })
+            if (rProps.length)
+              err.push('  ' + m.id + ' implements interface "' + i + '". This interface requires implementing properties: ' + rProps.substring(2))
+          }
+        }
+      })
+    }
+  }
+  if (m.required  &&  (m.required.constructor !== Array))
+    err.push('the required property required should be of type Array')
 
   return err
 }
