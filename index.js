6eb68dca0849b9a1703a4f3a7b177d4e2d4da7da
pgmemk <ellen.katsnelson@lablz.com>
2017-05-19 14:56:47 -0400

    separating custom models

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ exports.dictionaries = lang => {
   try {
     return require('./dictionary_' + lang + '.json')
   } catch (err) {
-    return require('./dictionary_en.json')
+    // return require('./dictionary_en.json')
   }
 }
 
 exports.dict = {
-  en: require('./dictionary_en.json'),
+  // en: require('./dictionary_en.json'),
   nl: require('./dictionary_nl.json')
 }
 // exports.formDefaults = require('./data/formDefaults.json')
