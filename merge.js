e3c2a779ae920eecc9cdc71dd3c87e59e856e72e
Mark Vayngrib <mark@tradle.io>
2017-06-08 19:26:36 -0400

    use pack-models

--- merge.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-const splitMerge = require('./split-merge')
-splitMerge.merge(process.argv[2], process.argv[3], true)
--- split.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-const path = require('path')
-const splitMerge = require('./split-merge')
-const models = require(path.resolve(process.argv[2]))
-splitMerge.split(models)
