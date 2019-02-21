ad1dd06ee6f6f759923a501c9cbb3848ceb0db9b
pgmemk <ellen@tradle.io>
2019-02-21 13:41:19 -0500

    feat: moving translations to S3

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict'
 exports.models = require('./models.json')
-exports.dictionaries = {
-//   get en() { return require('./dictionary_en.json') },
-  get nl() { return require('./dictionary_nl.json') },
-  get bn() { return require('./dictionary_bn.json') },
-  get vi() { return require('./dictionary_vi.json') },
-  get fr() { return require('./dictionary_fr.json') },
-  get es() { return require('./dictionary_es.json') },
-  get fil() { return require('./dictionary_fil.json') },
-}
+// exports.dictionaries = {
+// //   get en() { return require('./dictionary_en.json') },
+//   get nl() { return require('./dictionary_nl.json') },
+//   get bn() { return require('./dictionary_bn.json') },
+//   get vi() { return require('./dictionary_vi.json') },
+//   get fr() { return require('./dictionary_fr.json') },
+//   get es() { return require('./dictionary_es.json') },
+//   get fil() { return require('./dictionary_fil.json') },
+// }
 // exports.formDefaults = require('./data/formDefaults.json')
 exports.data = require('./data/data')
 exports.currencies = require('./data/tradle.Currency.json')
